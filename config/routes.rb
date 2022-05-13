5e2b8cf40f2f97ae45d677f7f3cfcc5b47029ffc
ClaudiaBlancoM <cabm0626@gmail.com>
2022-05-12 20:01:18 -0400

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+root 'main#home'
   # Defines the root path route ("/")
   # root "articles#index"
 end
